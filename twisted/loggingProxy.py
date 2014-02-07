bdaacc116dedb85840106b0916d6f065fc04bf4c
root <1991.daiki@gmail.com>
2014-02-08 01:00:44 +0900

    confirm some methods' output

--- twisted/loggingProxy.py
+++ twisted/loggingProxy.py
@@ -8,6 +8,17 @@ import sys
 class LoggingProxyRequest(proxy.ProxyRequest):
     def process(self):
         print "Request from %s for %s" % (self.getClientIP(), self.getAllHeaders()['host'])
+        #print "getAllHeader", self.getAllHeaders()
+        #print "getRequestHostname", self.getRequestHostname()
+#        print "getHost", self.getHost()#infomation of proxy server itself
+#        print "getClient", self.getClient()#connecting client e.g.) ~~no-Mac-Book-Pro
+#        print "getPassword", self.getPassword()#no output
+#        print "getHeader", self.getHeader()#this need key argument
+#        print "getCookie", self.getCookie()#this need key argument
+#        print "parseCookies", self.parseCookies()#only None
+#        print "getUser", self.getUser()#no output
+#        self.redirect("www.Google.com")#does not work well(I don't know how to use this)
+        
         try:
             proxy.ProxyRequest.process(self)
         except KeyError:
