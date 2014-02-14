d099ff6d0fb500b6165d81906c9ee3bc456f0973
Daiki Aminaka <1991.daiki@gmail.com>
2014-02-14 10:23:10 +0900

    I tried to use PeriodicCallback

--- tornado/websocketServer.py
+++ tornado/websocketServer.py
@@ -1,12 +1,13 @@
+import time
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+from tornado.ioloop import PeriodicCallback
 
 from tornado.options import define, options, parse_command_line
 
-define("port", default = 8080, help = "run on the given port", type = int)
 
-clients = dict()
+define("port", default = 8080, help = "run on the given port", type = int)
 
 class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
@@ -14,28 +15,31 @@ class IndexHandler(tornado.web.RequestHandler):
         self.write("This is your response")
         self.finish()
 
-class EchoWebSocket(tornado.websocket.WebSocketHandler):
+
+class SendWebSocket(tornado.websocket.WebSocketHandler):
     #on_message -> receive data
     #write_message -> send data
     def open(self):
-        #self.receive_message(self.on_message)
+        self.callback = PeriodicCallback(self._send_message, 400)
+        self.callback.start()
         print "WebSocket opend"
 
     def on_message(self, message):
-        #self.write_message(u'You said:' + message)
+        print message
 
-#    def write_message(self):
-        i  = 0
-        while i < 20:
-            self.write_message(str(i) + " \n")
-            i += 1
+
+    def _send_message(self):
+#        self.i += 1
+#        print i, self.i
+        self.write_message("test")
 
     def on_close(self):
+        self.callback.stop()
         print "WebSocket closed"
 
 app = tornado.web.Application([
     (r"/", IndexHandler),
-    (r"/ws", EchoWebSocket),
+    (r"/ws", SendWebSocket),
 ])
 
 if __name__ == "__main__":
